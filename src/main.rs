8922fb4ae6c176868e993e3caec7c8a3a61fed71
jpec57 <jean.paul.bella@hotmail.fr>
2020-01-28 23:19:34 +0100

    All working but need to see security of cors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use]
-extern crate rocket;
-
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+// #[macro_use] extern crate serde_derive;
 pub mod cors;
 
 use rocket::response::content;
+use rocket_contrib::json::{JsonValue};
+
 
 #[get("/")]
 fn index() -> &'static str {
@@ -23,12 +25,19 @@ fn json() -> content::Json<&'static str> {
     content::Json("{ 'hi': 'world' }")
 }
 
+
+#[get("/json")]
+fn get_json() -> JsonValue {
+    json!({
+        "id": 83,
+        "values": [1, 2, 3, 4]
+    })
+}
+
 fn main() {
     rocket::ignite()
-    .mount("/", routes![index])
-    .mount("/", routes![home_index])
-    .mount("/", routes![json])
-    // .attach(make_cors()) 
-    .attach(cors::CorsFairing) // Add this line
+    .mount("/", routes![index, home_index, json])
+    .mount("/", routes![get_json])
+    .attach(cors::CorsFairing)
     .launch();
 }
