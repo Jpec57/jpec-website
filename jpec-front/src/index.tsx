1537b64fa0d033284ea41550c41ab8f3a5041ee4
Jpec57 <jean.paul.bella@hotmail.fr>
2020-02-26 13:36:06 +0100

    Review header

--- jpec-front/src/index.tsx
+++ jpec-front/src/index.tsx
@@ -35,32 +35,17 @@ export default function App() {
         </div> */}
 <section id="header">
 					<div className="container">
-
-							<h1 id="logo"><a href="index.html">Strongly Typed</a></h1>
-							<p>A responsive HTML5 site template. Manufactured by HTML5 UP.</p>
+							<h1 id="logo"><a href="index.html">Jpec website</a></h1>
+							<p>This website is created to learn Rust and show you what I am currently fond of.</p>
 							<nav id="nav">
 								<ul>
-									<li><a className="icon solid fa-home" href="index.html"><span>Introduction</span></a></li>
+									<li><a className="icon solid fa-home" href="index.html">
+                    <span>Homepage</span></a></li>
 									<li>
-										<a href="#" className="icon fa-chart-bar"><span>Dropdown</span></a>
-										<ul>
-											<li><a href="#">Lorem ipsum dolor</a></li>
-											<li><a href="#">Magna phasellus</a></li>
-											<li><a href="#">Etiam dolore nisl</a></li>
-											<li>
-												<a href="#">Phasellus consequat</a>
-												<ul>
-													<li><a href="#">Magna phasellus</a></li>
-													<li><a href="#">Etiam dolore nisl</a></li>
-													<li><a href="#">Phasellus consequat</a></li>
-												</ul>
-											</li>
-											<li><a href="#">Veroeros feugiat</a></li>
-										</ul>
+										<a href="#" className="icon fa-globe">
+                      <span>Japanese</span></a>
 									</li>
-									<li><a className="icon solid fa-cog" href="left-sidebar.html"><span>Left Sidebar</span></a></li>
-									<li><a className="icon solid fa-retweet" href="right-sidebar.html"><span>Right Sidebar</span></a></li>
-									<li><a className="icon solid fa-sitemap" href="no-sidebar.html"><span>No Sidebar</span></a></li>
+									<li><a className="icon solid fa-calculator" href="left-sidebar.html"><span>Maths</span></a></li>
 								</ul>
 							</nav>
 
