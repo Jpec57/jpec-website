3a74adc7abf0dc015641c4d68a561e47142fc62b
Jpec57 <jean.paul.bella@hotmail.fr>
2020-03-13 13:51:48 +0100

    Merci Omar

--- jpec-front/src/pages/HomePage/HomePage.tsx
+++ jpec-front/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,17 @@
 import React, { useState } from 'react';
 const FirstPage: React.FC = () => {
   const [drawnNumber, setDrawnNumber] = useState(-1);
+
+  const renderResult = () => {
+    if (drawnNumber >= 0){
+      return (
+      <div>
+        You draw a <span>{drawnNumber}</span>
+      </div>);
+    }
+    return null;
+  };
+
   return (
     <div className="container">
       <header className="header-content">
@@ -14,10 +25,7 @@ const FirstPage: React.FC = () => {
         <button id="clear-button" className="btn btn-dark">Clear</button>
         <button id="predict-button" className="btn btn-dark">Predict</button>
       </div>
-      <div>
-
-        You draw a 
-      </div>
+      {renderResult()}
     </div>
   );
 }
