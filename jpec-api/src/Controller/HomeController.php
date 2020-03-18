25f107c214981487b6415755692dc0af45f404f1
Jpec57 <jean.paul.bella@hotmail.fr>
2020-03-18 21:14:34 +0100

    Using symfony in back

---
+++ jpec-api/src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HomeController.php',
+        ]);
+    }
+
+        /**
+     * @Route("/test", name="test")
+     */
+    public function test()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/HomeController.php',
+        ]);
+    }
+}
