2c029314461590dbc14888177df4bb78b6eae1a4
Jpec57 <jean.paul.bella@hotmail.fr>
2020-03-22 20:56:34 +0100

    Creating audio player

--- jpec-front/src/pages/Japanese/JapanesePage.tsx
+++ jpec-front/src/pages/Japanese/JapanesePage.tsx
@@ -1,6 +1,16 @@
-import React, {useRef, MutableRefObject} from 'react';
+import React, {useRef, MutableRefObject, useState, useEffect} from 'react';
+import './JapanesePage.scss';
+
+//https://www3.nhk.or.jp/news/easy/
 const JapanesePage: React.FC = () => {
-    var speechSynthesisVar: MutableRefObject<SpeechSynthesisUtterance> = useRef(new SpeechSynthesisUtterance());
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  if ('speechSynthesis' in window) {
+    console.log('Ok');
+  } else {
+    console.log('Not supported.');
+  }
+    var speechSynthesisVar: MutableRefObject<SpeechSynthesisUtterance | null> = useRef(null);
 
 
     const configureSpeechSynthesis = (rate: number = 1): SpeechSynthesisUtterance => {
@@ -13,15 +23,16 @@ const JapanesePage: React.FC = () => {
         return msg;
     };
 
-    // //TODO should only look once at page load but doesn't work
-    // useEffect(()=>{
-    //     speechSynthesisVar.current = configureSpeechSynthesis();
-    // }, []);
+    //TODO should only look once at page load but doesn't work
+    useEffect(()=>{
+        speechSynthesisVar.current = configureSpeechSynthesis();
+    }, []);
 
     const say = (message: string) => {
         speechSynthesisVar.current = configureSpeechSynthesis();
         speechSynthesisVar.current.text = message;
         window.speechSynthesis.speak(speechSynthesisVar.current);
+        // window.s
     };
 
   return (
@@ -30,8 +41,27 @@ const JapanesePage: React.FC = () => {
       </header>
       <div>
         <span>This is a test</span>
+        <div className="audio-player">
+        <div className="audio-controller-box">
+<div className="audio-rate">
+
+</div>
+<div className="centered-div">
+<i className="icon solid fa-play-circle" onClick={()=>{
+            say('234')
+            }}/>
+  </div>
+        </div>
+        <div className="answer-box">
+
+<input />
+        </div>
+        <div className="correction-box">
+
+        </div>
+        </div>
         <button onClick={()=>{
-            say('国内外の取材網を生かし、さまざまな分野のニュースをいち早く、正確にお伝えします')
+            say('234 国内外の取材網を生かし、さまざまな分野のニュースをいち早く、正確にお伝えします')
             }}>
             Click here
         </button>
